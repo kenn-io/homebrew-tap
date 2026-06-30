ff26725f1a89a125e3fad5d9eebcda18a87cedb5
Wes McKinney <wesm@apache.org>
2026-06-30 16:44:00 -0500

    Update roborev to v0.61.0 (#5)
    Updates the Homebrew formula to roborev v0.61.0 using the release tarball checksums from the release workflow.
    
    Co-authored-by: Wes McKinney <wesm@users.noreply.github.com>

--- Formula/roborev.rb
+++ Formula/roborev.rb
@@ -1,28 +1,28 @@
 class Roborev < Formula
   desc "Automatic code review daemon for git commits using AI agents"
   homepage "https://roborev.io"
-  version "0.60.0"
+  version "0.61.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/kenn-io/roborev/releases/download/v#{version}/roborev_#{version}_darwin_amd64.tar.gz"
-      sha256 "934a8a74cc18466f61acc737059e830f812775d3828f1bb8cdc6e9bfd807b1c9"
+      sha256 "5ad52af994bf94f7b87b287bee8812ff17ee1df740dee0eed9d9f602a62bc5bf"
     end
     if Hardware::CPU.arm?
       url "https://github.com/kenn-io/roborev/releases/download/v#{version}/roborev_#{version}_darwin_arm64.tar.gz"
-      sha256 "b40e92b4ac24f25b1a44a5756342842f79680c85c346120c1d6d7cb493eeb7ee"
+      sha256 "a42fdbbf8e95153900bd021cfa79266629459aba55d7bc4c1b69a18a7726a68b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/kenn-io/roborev/releases/download/v#{version}/roborev_#{version}_linux_amd64.tar.gz"
-      sha256 "f570f30c4151444039c4bc6a652391deb00587f59baec6744bd251a48908260f"
+      sha256 "d7506f219484a018e283b1cbc8aac47626cc5e35a389bedf456013ea613490f6"
     end
     if Hardware::CPU.arm?
       url "https://github.com/kenn-io/roborev/releases/download/v#{version}/roborev_#{version}_linux_arm64.tar.gz"
-      sha256 "ef490aeffb45ca18b2e0d0d4fcc44513750351bc9a4d6c50593e2f7d13b63d56"
+      sha256 "12099831776a64d32df85b806697eb7ff0d7334cbd0f11e5522b4f87fc3f2de1"
     end
   end
 
